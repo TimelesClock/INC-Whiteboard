4e147e63bbcc85e3615c0cb733625a14dc170dbe
Timeless <leongandy027@gmail.com>
2023-12-16 22:30:42 +0800

    backend

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -7,7 +7,7 @@ const app = express();
 const httpServer = createServer(app);
 const io = new Server(httpServer, {
   cors: {
-    origin: ['http://localhost:3000', 'http://127.0.0.1:5173'],
+    origin: "*",
   },
 });
 
