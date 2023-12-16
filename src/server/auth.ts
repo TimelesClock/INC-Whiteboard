de6ff96e4a179a96fa9e6a2aae197405f2bcf615
Timeless <leongandy027@gmail.com>
2023-12-16 22:50:06 +0800

    deploy

--- src/server/auth.ts
+++ src/server/auth.ts
@@ -4,9 +4,7 @@ import {
   type DefaultSession,
   type NextAuthOptions,
 } from "next-auth";
-import DiscordProvider from "next-auth/providers/discord";
 
-import { env } from "@/env";
 import { db } from "@/server/db";
 
 /**
