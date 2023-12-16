0afc17a844134648ad5bb6870673ba45eb6bf4e2
Timeless <leongandy027@gmail.com>
2023-12-17 01:06:04 +0800

    deploy

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -10,8 +10,9 @@ const app = express();
 
 const currentDir = process.cwd();
 
-const keyPath = path.join(currentDir, 'server.key');
-const certPath = path.join(currentDir, 'server.crt');
+const keyPath = path.join(path.resolve(currentDir, '..'), 'server.key');
+const certPath = path.join(path.resolve(currentDir, '..'), 'server.crt');
+
 
 const sslOptions = {
   key: fs.readFileSync(keyPath),
