e72843e8c978a975082758f95aea717a8ec148d0
Timeless <leongandy027@gmail.com>
2023-12-20 23:02:19 +0800

    changes

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const shell = require('shelljs');
 const https = require('https');
 const fs = require('fs');
+const { spawn } = require('child_process');
 
 const app = express();
 const port = 5000; // HTTPS port
@@ -12,28 +12,31 @@ app.use(bodyParser.json()); // For parsing application/json
 // Read the SSL certificate and private key
 const privateKey = fs.readFileSync('server.key', 'utf8');
 const certificate = fs.readFileSync('server.crt', 'utf8');
-
 const credentials = { key: privateKey, cert: certificate };
 
 // Endpoint to receive webhooks from GitHub
 app.post('/deploy', (req, res) => {
-    const payload = req.body;
-
     // Optional: Verify payload with your secret token for security
 
-    // The deployment script
-    try {
-        console.log('Deployment started...');
-        shell.exec("cd /home/ubuntu/INC-TLDraw-Socket/backend")
-        shell.exec('git pull origin main');
-        shell.exec('sudo docker build -t inc-whiteboard-socket .');
-        shell.exec('sudo docker run -d -p 3001:3001 inc-tldraw-socket');
-        console.log('Deployment successful!');
-        res.status(200).send('Deployment successful!');
-    } catch (error) {
-        console.error('Deployment failed:', error);
-        res.status(500).send('Deployment failed');
-    }
+    console.log('Deployment started...');
+
+    // Send the response immediately
+    res.status(200).send('Deployment started!');
+
+    // Spawn a new process for deployment
+    const deployScript = spawn('sh', ['./deploy_script.sh']);
+
+    deployScript.stdout.on('data', (data) => {
+        console.log(`stdout: ${data}`);
+    });
+
+    deployScript.stderr.on('data', (data) => {
+        console.error(`stderr: ${data}`);
+    });
+
+    deployScript.on('close', (code) => {
+        console.log(`Deployment script exited with code ${code}`);
+    });
 });
 
 // Create HTTPS server
@@ -43,3 +46,4 @@ httpsServer.listen(port, () => {
     console.log(`HTTPS Server is listening on port ${port}`);
 });
 
+// Don't forget to create the deploy_script.sh as described previously
